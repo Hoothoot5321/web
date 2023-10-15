6b1ed2f78145fe110573b777f901d5e6a288c36a
Hoothoot5321 <martinjens986@gmail.com>
2023-10-15 19:38:15 +0200

    asdg

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+import adapter from "@sveltejs/adapter-node";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+  kit: {
+    adapter: adapter(),
+  },
   preprocess: vitePreprocess(),
-}
+};
